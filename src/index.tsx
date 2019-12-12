c0da8be05a133e4fce9ffd8f89837847a2c2c0ae
machao <abcd1234>
2019-12-12 17:24:12 +0800

    添加eslint、ts支持

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+// 引入 polyfill 兼容到 ie9
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
