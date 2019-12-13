1fa366d896914a6fb0cd88de7632efe0e66b4208
machao <abcd1234>
2019-12-13 18:59:52 +0800

    form

---
+++ src/pages/components/MyForm.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+export class MyForm extends Component {
+    componentDidMount(): void {
+
+    }
+
+    render() {
+        return (
+            <form>
+                <input/>
+            </form>
+        );
+    }
+}
