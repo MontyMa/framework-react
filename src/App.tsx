9f231122aeaad497cec22c548475db1206ab668b
monty <montyma1207@gmail.com>
2019-12-17 00:01:08 +0800

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export class App extends Component<any, {
     render(): React.ReactElement {
         return (
             <div className="App">
-                <h1>Hello World</h1>
+                <h1>Hello World121312</h1>
                 <StateAndPropType hello={this.state.appName}
                     handler={this.stateAndPropTypeHandler}
                 />
